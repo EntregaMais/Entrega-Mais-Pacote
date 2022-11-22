7a45a0819749906e4bbf83f28d97796bbd991c98
adrianaalbuquerque <80019208+adrianaalbuquerque@users.noreply.github.com>
2022-11-22 11:52:42 -0300

    feat: Adicionando método de teste para salvarPacote()

--- src/test/java/com/entrega/mais/produto/service/PacoteServiceTest.java
+++ src/test/java/com/entrega/mais/produto/service/PacoteServiceTest.java
@@ -1,5 +1,6 @@
 package com.entrega.mais.produto.service;
-
+import com.entrega.mais.produto.model.Pacote;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,6 +9,7 @@ class PacoteServiceTest {
 
     @Test
     void encontraPacotePorId() {
+
     }
 
     @Test
@@ -24,6 +26,12 @@ class PacoteServiceTest {
 
     @Test
     void salvarPacote() {
+        Pacote pacote = new Pacote (Long.valueOf(1), Long.valueOf(123), "Moda Center", "88997322", "M", "Pedido de Teste");
+        PacoteService pacoteService = new PacoteService();
+        //Assertions.assertEquals(pacoteService.salvarPacote(pacote));
+
+        Assertions.assertNotNull(pacote);  //Checks for null
+        Assertions.assertInstanceOf(PacoteService.class, pacoteService.salvarPacote(pacote));
     }
 
     @Test
