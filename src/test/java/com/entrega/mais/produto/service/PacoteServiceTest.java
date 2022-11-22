70460a83b5ef2a7e3ec1a55eb5735f24c4238f41
adrianaalbuquerque <80019208+adrianaalbuquerque@users.noreply.github.com>
2022-11-22 10:02:33 -0300

    feat: adicionando JUnit  5.9.1 e criando classe de teste

---
+++ src/test/java/com/entrega/mais/produto/service/PacoteServiceTest.java
@@ -0,0 +1,32 @@
+package com.entrega.mais.produto.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PacoteServiceTest {
+
+    @Test
+    void encontraPacotePorId() {
+    }
+
+    @Test
+    void encontraPacotePorCdPedido() {
+    }
+
+    @Test
+    void encontraPacotesPorFornecedor() {
+    }
+
+    @Test
+    void encontraPacotePorTamanho() {
+    }
+
+    @Test
+    void salvarPacote() {
+    }
+
+    @Test
+    void atualizarPacote() {
+    }
+}
