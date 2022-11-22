0f348c4864521b0c682df4bf2b3bede8beaa1673
adrianaalbuquerque <80019208+adrianaalbuquerque@users.noreply.github.com>
2022-11-22 13:17:32 -0300

    feat: atualização classe de teste PacoteService

--- src/test/java/com/entrega/mais/produto/service/PacoteServiceTest.java
+++ src/test/java/com/entrega/mais/produto/service/PacoteServiceTest.java
@@ -1,11 +1,19 @@
 package com.entrega.mais.produto.service;
 import com.entrega.mais.produto.model.Pacote;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class PacoteServiceTest {
+
+    @Autowired
+    PacoteService pacoteService;
+    Pacote pacote = new Pacote (Long.valueOf(1), Long.valueOf(123), "Moda Center", "88997322", "M", "Pedido de Teste");
 
     @Test
     void encontraPacotePorId() {
@@ -26,14 +34,9 @@ class PacoteServiceTest {
 
     @Test
     void salvarPacote() {
-        Pacote pacote = new Pacote (Long.valueOf(1), Long.valueOf(123), "Moda Center", "88997322", "M", "Pedido de Teste");
-        PacoteService pacoteService = new PacoteService();
-        //Assertions.assertEquals(pacoteService.salvarPacote(pacote));
-
-        Assertions.assertNotNull(pacote);  //Checks for null
-        Assertions.assertInstanceOf(PacoteService.class, pacoteService.salvarPacote(pacote));
+        Assertions.assertNotNull(pacote);
+        Assertions.assertInstanceOf(Pacote.class, pacoteService.salvarPacote(pacote));
     }
-
     @Test
     void atualizarPacote() {
     }
