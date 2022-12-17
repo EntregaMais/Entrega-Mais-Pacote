ba53262bc9591010d3f5a72ba3d8b70339bfa41f
Diego Figueiredo <diegofigmedeiros@gmail.com>
2022-12-17 18:10:35 -0300

    reformat: removido teste do projeto pacote

--- src/test/java/com/entrega/mais/produto/service/PacoteServiceTest.java
+++ src/test/java/com/entrega/mais/produto/service/PacoteServiceTest.java
@@ -11,33 +11,33 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class PacoteServiceTest {
 
-    @Autowired
-    PacoteService pacoteService;
-    Pacote pacote = new Pacote (Long.valueOf(1), Long.valueOf(123), "Moda Center", "88997322", "M", "Pedido de Teste");
+    // @Autowired
+    // PacoteService pacoteService;
+    // Pacote pacote = new Pacote (Long.valueOf(1), Long.valueOf(123), "Moda Center", "88997322", "M", "Pedido de Teste");
 
-    @Test
-    void encontraPacotePorId() {
+    // @Test
+    // void encontraPacotePorId() {
 
-    }
+    // }
 
-    @Test
-    void encontraPacotePorCdPedido() {
-    }
+    // @Test
+    // void encontraPacotePorCdPedido() {
+    // }
 
-    @Test
-    void encontraPacotesPorFornecedor() {
-    }
+    // @Test
+    // void encontraPacotesPorFornecedor() {
+    // }
 
-    @Test
-    void encontraPacotePorTamanho() {
-    }
+    // @Test
+    // void encontraPacotePorTamanho() {
+    // }
 
-    @Test
-    void salvarPacote() {
-        Assertions.assertNotNull(pacote);
-        Assertions.assertInstanceOf(Pacote.class, pacoteService.salvarPacote(pacote));
-    }
-    @Test
-    void atualizarPacote() {
-    }
+    // @Test
+    // void salvarPacote() {
+    //     Assertions.assertNotNull(pacote);
+    //     Assertions.assertInstanceOf(Pacote.class, pacoteService.salvarPacote(pacote));
+    // }
+    // @Test
+    // void atualizarPacote() {
+    // }
 }
